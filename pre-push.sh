0ead9e8264368c1512aa8baeda1f63443f5a3095
Aman Lalpuria <Lalpuria_Am@India.XoriantCorp.com>
2019-11-13 10:55:41 +0530

    testing:

--- pre-push.sh
+++ pre-push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo "This is pre-push check"
 
 # command to build the project. and count the error 
-echo "This is pre-push check"
+# grep to search need to be used
 number_of_errors = 2
 
 if [ $number_of_errors != '0' ]; then
-  echo "$num_of_failures tests have failed. You cannot commit until all project build successfully."
+  echo "$number_of_errors tests have failed. You cannot commit until the project build successfully."
   exit 1
 fi
