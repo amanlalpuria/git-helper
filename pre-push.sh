d5528452a2c1433b3a1aa575feabdd20650d4163
Aman Lalpuria <Lalpuria_Am@India.XoriantCorp.com>
2019-11-13 08:54:53 +0530

    JS file

--- pre-push.sh
+++ pre-push.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+# grep is to searching plain text
+# sed Stream editor for filtering and transforming text
+
 current_branch=`git branch | grep '*' | sed 's/* //'`
 
 if [ "$current_branch" = "master" ]; then
     echo "You are about to push on branch. Lets try to build the code first"
     
     # code build statement
-
+    node index.js
 
     if [ $? -eq 0 ]; then
         # tests passed, proceed to prepare commit message
@@ -13,7 +16,6 @@ if [ "$current_branch" = "master" ]; then
     else
         # some tests failed, prevent from committing broken code on master
         echo "Some tests failed. You are not allowed to commit broken code on master! Aborting the commit."
-        echo "Note: you can still commit broken code on feature branches"
         exit 1
     fi
 fi
