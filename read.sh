23f001fdfa97e593a5da070dfa653e4db822ee33
Aman Lalpuria <Lalpuria_Am@India.XoriantCorp.com>
2019-11-28 11:04:22 +0530

    Updated for different error

--- read.sh
+++ read.sh
@@ -8,35 +8,37 @@ filename='file.xml'
 AuxClasspathEntry=0
 ClassStats=0
 ClassProfile=0
+bigError='AuxClasspathEntry'
+mediumError='ClassStats'
+smallError='ClassProfile'
 
 # reading each line
 while read line; do
 
-# if error is of type AuxClasspathEntry
-#if [ $line ?? 'AuxClasspathEntry' ]; then
-#  echo "It's there!"
-#  AuxClasspathEntry=$((AuxClasspathEntry+1))
-#fi
 
-if $line | grep 'AuxClasspathEntry'; then
+if [[ "$line" == *"$bigError"* ]]; then
+  echo "$line"
   AuxClasspathEntry=$((AuxClasspathEntry+1))
 fi
 
 
-if echo "$line" | grep 'ClassStats'; then
-  ClassStats=$((ClassStats+1))
+if [[ "$line" == *"$mediumError"* ]]; then
+ echo "$line"
+ ClassStats=$((ClassStats+1))
 fi
 
 
-if echo "$line" | grep 'ClassProfile'; then
-  ClassProfile=$((ClassProfile+1))
+if [[ "$line" == *"$smallError"* ]]; then
+ echo "$line"
+ ClassProfile=$((ClassProfile+1))
 fi
 
 done < $filename
 
 # Final Count
 echo "Errors in the project :"
-echo "1. AuxClasspathEntry - " > $AuxClasspathEntry
-echo "2. ClassStats        - " > $ClassStats
-echo "3. ClassProfile      - " > $ClassProfile
+echo "1. AuxClasspathEntry - $AuxClasspathEntry"
+echo "2. ClassStats        - $ClassStats"
+echo "3. ClassProfile      - $ClassProfile"
 
+echo "*************************************************************"
