b0cb47bc513027da2e22f2639929e669aac5e9bb
root <root@citi-icg-harshalp-req.112624.novalocal>
2019-11-28 03:56:19 +0000

    Worked Script

--- read.sh
+++ read.sh
@@ -3,15 +3,35 @@
 #declaring variables
 filename='file.xml'
 AuxClasspathEntry=0
+ClassStats=0
+ClassProfile=0
 
 # reading each line
 while read line; do
 
 # if error is of type AuxClasspathEntry
-if [ $line ?? 'AuxClasspathEntry' ]; then
+#if [ $line ?? 'AuxClasspathEntry' ]; then
+#  echo "It's there!"
+#  AuxClasspathEntry=$((AuxClasspathEntry+1))
+#fi
+
+if echo "$line" | grep 'AuxClasspathEntry'; then
   echo "It's there!"
   AuxClasspathEntry=$((AuxClasspathEntry+1))
 fi
+
+
+if echo "$line" | grep 'AuxClasspathEntry'; then
+  echo "It's there!"
+  AuxClasspathEntry=$((AuxClasspathEntry+1))
+fi
+
+
+if echo "$line" | grep 'AuxClasspathEntry'; then
+  echo "It's there!"
+  AuxClasspathEntry=$((AuxClasspathEntry+1))
+fi
+
 done < $filename
 
 # Final Count
