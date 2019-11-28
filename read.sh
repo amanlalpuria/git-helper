38e955442af92ec8aa936e2aa278e141bf0827fe
root <root@citi-icg-harshalp-req.112624.novalocal>
2019-11-28 05:33:26 +0000

    Updated search command

--- read.sh
+++ read.sh
@@ -8,35 +8,31 @@ filename='file.xml'
 AuxClasspathEntry=0
 ClassStats=0
 ClassProfile=0
+key_to_find='AuxClasspathEntry'
 
 # reading each line
 while read line; do
 
-# if error is of type AuxClasspathEntry
-#if [ $line ?? 'AuxClasspathEntry' ]; then
-#  echo "It's there!"
-#  AuxClasspathEntry=$((AuxClasspathEntry+1))
-#fi
 
-if $line | grep 'AuxClasspathEntry'; then
+if [[ "$line" == *"$key_to_find"* ]]; then
   AuxClasspathEntry=$((AuxClasspathEntry+1))
 fi
 
 
-if echo "$line" | grep 'ClassStats'; then
-  ClassStats=$((ClassStats+1))
-fi
+#if echo "$line" | grep 'ClassStats'; then
+#  ClassStats=$((ClassStats+1))
+#fi
 
 
-if echo "$line" | grep 'ClassProfile'; then
-  ClassProfile=$((ClassProfile+1))
-fi
+#if echo "$line" | grep 'ClassProfile'; then
+#  ClassProfile=$((ClassProfile+1))
+#fi
 
 done < $filename
 
 # Final Count
 echo "Errors in the project :"
-echo "1. AuxClasspathEntry - " > $AuxClasspathEntry
-echo "2. ClassStats        - " > $ClassStats
-echo "3. ClassProfile      - " > $ClassProfile
+echo "1. AuxClasspathEntry - $AuxClasspathEntry"
+echo "2. ClassStats        - $ClassStats"
+echo "3. ClassProfile      - $ClassProfile"
 
