163f4f8dbe82337a705d04787ce00c5a40d257fb
Aman Lalpuria <Lalpuria_Am@India.XoriantCorp.com>
2019-11-29 14:46:47 +0530

    Added Maven project

---
+++ Java/src/main/java/com/example/gitissue/GitIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitIssueApplication.class, args);
+	}
+
+}
--- Javascript/read.sh
+++ Javascript/read.sh
@@ -41,8 +41,4 @@ echo "1. AuxClasspathEntry - $AuxClasspathEntry"
 echo "2. ClassStats        - $ClassStats"
 echo "3. ClassProfile      - $ClassProfile"
 
-<<<<<<< HEAD
 echo "*************************************************************"
-=======
-echo "*************************************************************"
->>>>>>> 708bb3a388e85aac51feff5f52d3bfa479fb2c76
